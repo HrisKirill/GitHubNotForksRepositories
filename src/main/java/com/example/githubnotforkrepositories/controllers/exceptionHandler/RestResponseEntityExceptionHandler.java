8a71e73dc2518344076ed1731c3ef351da94cf79
HrisKirill <hristoforow.kirill@gmail.com>
2023-09-02 20:21:54 +0300

    add json header when handle UnsupportedMediaTypeException

--- src/main/java/com/example/githubnotforkrepositories/controllers/exceptionHandler/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/githubnotforkrepositories/controllers/exceptionHandler/RestResponseEntityExceptionHandler.java
@@ -3,12 +3,14 @@ package com.example.githubnotforkrepositories.controllers.exceptionHandler;
 import com.example.githubnotforkrepositories.exceptions.UnsupportedMediaTypeException;
 import com.example.githubnotforkrepositories.exceptions.UserNotFoundException;
 import com.example.githubnotforkrepositories.models.Response;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestResponseEntityExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
@@ -18,9 +20,11 @@ public class RestResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(UnsupportedMediaTypeException.class)
-    public ResponseEntity<Response> handleException(Exception e) {
+    public ResponseEntity<Response> handleUnsupportedMediaTypeException(Exception e) {
         Response response = new Response(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
-        return new ResponseEntity<>(response, response.getStatus());
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<>(response, responseHeaders, response.getStatus());
     }
 
 }
