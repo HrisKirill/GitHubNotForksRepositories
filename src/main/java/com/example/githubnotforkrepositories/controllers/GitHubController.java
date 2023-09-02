6047532a8ef5166cc939aa9c68143a2f36e69c6f
HrisKirill <hristoforow.kirill@gmail.com>
2023-09-02 18:52:29 +0300

    add controller

---
+++ src/main/java/com/example/githubnotforkrepositories/controllers/GitHubController.java
@@ -0,0 +1,55 @@
+package com.example.githubnotforkrepositories.controllers;
+
+import com.example.githubnotforkrepositories.dto.GitHubRepositoryDTO;
+import com.example.githubnotforkrepositories.mappers.MapModelToDTO;
+import com.example.githubnotforkrepositories.models.GitHubRepository;
+import com.example.githubnotforkrepositories.services.BranchService;
+import com.example.githubnotforkrepositories.services.GitHubRepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("github/api")
+public class GitHubController {
+
+    private final GitHubRepositoryService gitHubRepositoryService;
+    private final BranchService branchService;
+    private final MapModelToDTO mapper;
+
+    @Autowired
+    public GitHubController(GitHubRepositoryService gitHubRepositoryService, BranchService branchService, MapModelToDTO mapper) {
+        this.gitHubRepositoryService = gitHubRepositoryService;
+        this.branchService = branchService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping(value = "/repositories/{userName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<GitHubRepositoryDTO>> getRepositories(
+            @PathVariable String userName) {
+
+        List<GitHubRepository> gitHubRepositories = gitHubRepositoryService.
+                getUserRepositories(userName);
+
+        if (gitHubRepositories.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        List<GitHubRepositoryDTO> repositoryDTOS = gitHubRepositoryService.
+                getUserNotForkRepositories(gitHubRepositories).stream()
+                .map(rep -> mapper.mapToRepositoryInfo(
+                        rep,
+                        branchService.getBranches(
+                                rep.getOwner().getLogin(),
+                                rep.getName()
+                        )
+                ))
+                .toList();
+
+
+        return ResponseEntity.ok(repositoryDTOS);
+    }
+}
