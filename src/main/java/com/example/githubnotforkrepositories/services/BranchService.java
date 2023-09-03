c31c1b56c7882f6faa693845eaa4ed7eb0a8c589
HrisKirill <hristoforow.kirill@gmail.com>
2023-09-03 11:01:45 +0300

    rename exception

--- src/main/java/com/example/githubnotforkrepositories/services/BranchService.java
+++ src/main/java/com/example/githubnotforkrepositories/services/BranchService.java
@@ -1,9 +1,11 @@
 package com.example.githubnotforkrepositories.services;
 
+import com.example.githubnotforkrepositories.exceptions.ModelNotFoundException;
 import com.example.githubnotforkrepositories.models.Branch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -21,17 +23,21 @@ public class BranchService {
     }
 
     public List<Branch> getBranches(String userName, String repositoryName) {
-        Branch[] branches = restTemplate.getForObject(
-                "https://api.github.com/repos/{username}/{repoName}/branches",
-                Branch[].class,
-                userName,
-                repositoryName
-        );
+        try {
+            Branch[] branches = restTemplate.getForObject(
+                    "https://api.github.com/repos/{username}/{repoName}/branches",
+                    Branch[].class,
+                    userName,
+                    repositoryName
+            );
 
-        if (branches != null) {
-            return Arrays.asList(branches);
-        } else {
-            return Collections.emptyList();
+            if (branches != null) {
+                return Arrays.asList(branches);
+            } else {
+                return Collections.emptyList();
+            }
+        } catch (HttpClientErrorException.NotFound exception) {
+            throw new ModelNotFoundException("User or repository not found");
         }
     }
 }
